76205bc6c0ba5c375444cb66ad614d923fa3b4fb
Nangialai <waziri.nangialai@gmail.com>
2021-09-01 14:41:28 +0200

    setup project

---
+++ backend-DonDiablo/src/main/java/nl/novi/backenddondiablo/backendDonDiablo/BackendDonDiabloApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.backenddondiablo.backendDonDiablo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDonDiabloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDonDiabloApplication.class, args);
+	}
+
+}
