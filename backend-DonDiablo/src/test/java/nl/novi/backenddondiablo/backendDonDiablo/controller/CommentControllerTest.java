2de9df0563f6b6d6a1d92580e30d696e667ff099
Nangialai <waziri.nangialai@gmail.com>
2021-10-31 13:41:35 +0100

    created unit tests, for musicFileRepository, Utils and helpers

---
+++ backend-DonDiablo/src/test/java/nl/novi/backenddondiablo/backendDonDiablo/controller/CommentControllerTest.java
@@ -0,0 +1,16 @@
+package nl.novi.backenddondiablo.backendDonDiablo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentControllerTest {
+
+    @Test
+    void getComment() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+}
---
+++ backend-DonDiablo/src/test/java/nl/novi/backenddondiablo/backendDonDiablo/service/MusicFileServiceImplementationTest.java
@@ -0,0 +1,56 @@
+package nl.novi.backenddondiablo.backendDonDiablo.service;
+
+import nl.novi.backenddondiablo.backendDonDiablo.BackendDonDiabloApplication;
+import nl.novi.backenddondiablo.backendDonDiablo.model.Comment;
+import nl.novi.backenddondiablo.backendDonDiablo.model.MusicFile;
+import nl.novi.backenddondiablo.backendDonDiablo.repository.MusicFileRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.junit.jupiter.api.Assertions.*;
+//@SpringBootTest
+//@ContextConfiguration(classes = {BackendDonDiabloApplication.class})
+@ExtendWith(MockitoExtension.class)
+public class MusicFileServiceImplementationTest {
+    @InjectMocks
+    private MusicFileServiceImplementation musicFileService;
+
+    @Mock
+    private MusicFileRepository musicFileRepository;
+
+    @Mock
+    MusicFile musicFile;
+
+//    @BeforeEach
+//    void setUp() {
+//    }
+
+    @Test
+    public void GetMusicFile() {
+        musicFile = new MusicFile();
+        Comment comment = new Comment("Empty");
+        musicFile.setUploadDate("2021/10/29");
+        musicFile.setFileName("test.mp3");
+        musicFile.setUploader("nang");
+        musicFile.setComment(comment);
+
+        Mockito
+                .when(musicFileRepository.getMusicFileByUploader(musicFile.getUploader()))
+                .thenReturn(musicFile);
+
+        MusicFile expected = musicFile;
+        MusicFile found = musicFileService.getMusicFile(musicFile.getUploader());
+
+        assertEquals(expected, found);
+    }
+
+}
